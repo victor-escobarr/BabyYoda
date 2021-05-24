89631704cd1993ff073ac07e3698900da77a557b
pato <patricio.ad26301@gmail.com>
2021-05-23 20:21:07 -0500

    JS primera animacion

--- Act6/Act6.js
+++ Act6/Act6.js
@@ -1,3 +1,4 @@
+let j=0;
 $(document).ready(function(){
     console.log("hola")
     $("#boton1").on("click",()=>{
@@ -11,5 +12,18 @@ $(document).ready(function(){
     })
     $("#boton4").on("click",()=>{
         $("#cup").css("backgroundColor", "chocolate");
+    })
+    $("#boton5").on("click",()=>{
+        if(j%2==0){
+            $("#mangaright").css("animation", "comer 5s ease-in infinite running");
+            $("#manoIzq").css("animation", "comer1 5s ease-in infinite running");
+            $("#comida").css("animation", "comer2 5s ease-in infinite running");
+        }
+        else{
+            $("#mangaright").css("animation", "comer 5s ease-in infinite paused");
+            $("#manoIzq").css("animation", "comer1 5s ease-in infinite paused");
+            $("#comida").css("animation", "comer2 5s ease-in infinite paused"); 
+        }
+        j++;
     })
 });
