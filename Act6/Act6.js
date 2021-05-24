48e56906577a598c02195c95d812015a755ee870
jaz-ramirez <jazmin.ramirez@comunidad.unam.mx>
2021-05-24 00:44:33 -0500

    FEAT: Animación dormir lista;

--- Act6/Act6.js
+++ Act6/Act6.js
@@ -13,3 +13,17 @@ $(document).ready(function(){
         $("#cup").css("backgroundColor", "chocolate");
     })
 });
+
+
+$("#boton6").on ("click", ()=>
+  {
+    console.log("click");
+    
+    $(".ojo").attr("class", "eyes");
+    $("#izq").attr("id", "eyeleft");
+    $("#der").attr("id", "eyeright");
+
+    var elementos = $('<section id="sleep"><div id="upzeta"></div><div id="middlezeta"></div><div id="downzeta"></div><div id="uz2"></div><div id="mz2"></div><div id="dz2"></div><div id="bigz"></div><div id="granz"></div><div id="largez"></div></section>');
+    $("#cuerpo").after(elementos);
+
+});
