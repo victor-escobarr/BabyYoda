6234eedb064b79545462b195997cb696c387ac09
pato <patricio.ad26301@gmail.com>
2021-05-23 20:14:12 -0500

    FEAT: JS primeras funciones

--- Act6/Act6.js
+++ Act6/Act6.js
@@ -1,1 +1,15 @@
-Act6
+$(document).ready(function(){
+    console.log("hola")
+    $("#boton1").on("click",()=>{
+        $("#cup").css("backgroundColor", "red");
+    })
+    $("#boton2").on("click",()=>{
+        $("#cup").css("backgroundColor", "rgb(36, 16, 122)");
+    })
+    $("#boton3").on("click",()=>{
+        $("#cup").css("backgroundColor", "gold");
+    })
+    $("#boton4").on("click",()=>{
+        $("#cup").css("backgroundColor", "chocolate");
+    })
+});
